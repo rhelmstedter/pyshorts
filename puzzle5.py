fe26aa2b30b5aaaeaf4ddb5ff3b8713c97e2aeac
Russell Helmstedter <rhelmstedter@gmail.com>
2022-06-05 20:45:13 -0700

    add doc string

--- puzzle5.py
+++ puzzle5.py
@@ -1,5 +1,20 @@
-"""my solution"""
+"""
+Puzzle 5
+1. It is a thre-digit whole number.
+2. It is divisible by 4.
+3. It is divisible by 5.
+4. It is divisible by 11.
+5. It is divisible by 3.
+6. It is divisible by 10.
+7. It is divisible by 6.
+8. It not is divisible by 9.
+9. It not is divisible by 8.
+10. It not is divisible by 7.
+"""
+
 from math import lcm
+
+"""my solution"""
 for n in range(100, 1000):
     if not n % lcm(3, 4, 5, 6, 10, 11) \
             and n % lcm(7, 8, 9):
